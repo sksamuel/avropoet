11f0d3e017b73aca5b3795b7b23ac9e99732b93d
sksamuel <sam@sksamuel.com>
2021-08-08 19:46:19 -0500

    Tweaked gradle versioning

--- buildSrc/src/main/kotlin/Ci.kt
+++ buildSrc/src/main/kotlin/Ci.kt
@@ -2,14 +2,14 @@ object Ci {
 
    // this is the version used for building snapshots
    // .GITHUB_RUN_NUMBER-snapshot will be appended
-   private const val snapshotBase = "1.0.0"
+   private const val snapshotBase = "1.0"
    const val org = "com.sksamuel.avropoet"
 
    private val githubRunNumber = System.getenv("GITHUB_RUN_NUMBER")
 
    private val snapshotVersion = when (githubRunNumber) {
-      null -> "$snapshotBase-LOCAL"
-      else -> "$snapshotBase.${githubRunNumber}-SNAPSHOT"
+      null -> "$snapshotBase.LOCAL"
+      else -> "$snapshotBase.0.${githubRunNumber}-SNAPSHOT"
    }
 
    val gradleVersion = when (githubRunNumber) {
